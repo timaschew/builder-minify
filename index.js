9d3bf41f90171992346fbcda0edcc2aae8ed8361
Anton Wilhelm <timaschew@gmail.com>
2014-08-11 12:10:39 +0200

    fix syntax error

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var minify = require('minify');
 
-exports = module.exports = function (options) {
-
-  return function js(file, done) {
-    file.read(function (err, string) {
-      if (err) return done(err);
-      file.string = minify.js(string);
-      done()
-    });
-  });
-
-  return function css(file, done) {
-    file.read(function (err, string) {
-      if (err) return done(err);
-      file.string = minify.css(string);
-      done()
-    });
-  });
+module.exports = function(options) {
+  return {
+    js: function(file, done) {
+      return file.read(function(err, string) {
+        if (err) return done(err);
+        file.string = minify.js(string);
+        return done();
+      });
+    },
+    css: function(file, done) {
+      return file.read(function(err, string) {
+        if (err) return done(err);
+        file.string = minify.css(string);
+        return done();
+      });
+    }
+  };
+};
