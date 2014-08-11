ec95444e2ef72f27e131203c6c90b9d67bb65979
Anton Wilhelm <timaschew@gmail.com>
2014-08-11 13:39:37 +0200

    works now

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var minify = require('minify');
 
-module.exports = function(options) {
-  return {
-    js: function(file, done) {
-      return file.read(function(err, string) {
-        if (err) return done(err);
-        file.string = minify.js(string);
-        return done();
-      });
-    },
-    css: function(file, done) {
-      return file.read(function(err, string) {
-        if (err) return done(err);
-        file.string = minify.css(string);
-        return done();
-      });
-    }
+module.exports.js = function(options) {
+  return function js(file, done) {
+    return file.read(function(err, string) {
+      if (err) return done(err);
+      file.string = minify.js(string);
+      return done();
+    });
+  };
+};
+
+module.exports.css = function(options) {
+  return function css(file, done) {
+    return file.read(function(err, string) {
+      if (err) return done(err);
+      file.string = minify.css(string);
+      return done();
+    });
   };
 };
